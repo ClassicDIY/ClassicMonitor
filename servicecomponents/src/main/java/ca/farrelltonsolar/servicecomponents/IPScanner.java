10f68007cff2800be7d5d0810b0fdd13ad62b40e
graham22 <graham.a.ross@gmail.com>
2015-06-29 12:29:01 -0400

    Updated intro

---
+++ servicecomponents/src/main/java/ca/farrelltonsolar/servicecomponents/IPScanner.java
@@ -0,0 +1,12 @@
+package ca.farrelltonsolar.servicecomponents;
+
+import ca.farrelltonsolar.j2modlite.Modbus;
+
+/**
+ * Created by Graham on 08/12/2014.
+ */
+public class IPScanner {
+
+    private int _port = Modbus.DEFAULT_PORT;
+
+}
